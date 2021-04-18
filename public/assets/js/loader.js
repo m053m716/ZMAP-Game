2fa270f9f986dfb31a2c5725219312339e427de1
Glitch (hello-express) <none>
2021-04-18 19:48:03 +0000

    🛣🚲 Checkpoint ./public/assets/js/loader.js:26910881/2788 ./views/index.html:26910881/2206 ./public/assets/css/style.css:26910881/38

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -9,25 +9,33 @@ class Loader {
               <h1>ZMAP 5e Campaign</h1>
               <h3>Saltmarsh: Dead Men Tell No Tales</h3>
               <hr class="intro-divider" />
-                <ul class="list-inline intro-social-buttons">
-                  <li>
-                    <a href="https://zmap-game.glitch.me/characters" class="btn btn-default btn-lg"><span class="network-name">Characters</span></a>
-                  </li>
-                  <li>
-                    <a href="https://zmap-game.glitch.me/profile/login" class="btn btn-default btn-lg"><span class="network-name">Profile</span></a                  >
-                  </li>
-                  <li>
-                    <a href="https://zmap-game.glitch.me/game" class="btn btn-default btn-lg"><span class="network-name">Play</span></a>
-                  </li>
-                </ul>
+                <ul class="list-inline"></ul>
             </div>
           </div>
         </div>
       </div>
       `;
-    console.log("Writing new Banner HTML:");
-    console.log(new_str);
+    // console.log("Writing new Banner HTML:");
+    // console.log(new_str);
     document.getElementById("about").innerHTML = new_str;
+    
+  }
+  addButtons(document) {
+    const new_str = `
+      <li>
+        <a href="https://zmap-game.glitch.me/characters" class="btn btn-default btn-lg"><span class="network-name">Characters</span></a>
+      </li>
+      <li>
+        <a href="https://zmap-game.glitch.me/profile/login" class="btn btn-default btn-lg"><span class="network-name">Profile</span></a                  >
+      </li>
+      <li>
+        <a href="https://zmap-game.glitch.me/game" class="btn btn-default btn-lg"><span class="network-name">Play</span></a>
+      </li>
+    `
+    const u = document.getElementsByClassName('list-inline');
+    for (var i = 0; i < u.length; i++) {
+      u[i].innerHTML = new_str;
+    } 
   }
 }
 const loader = new Loader();
