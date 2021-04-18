68ebb10d2f69d4bdc2a08563fa51bed97d442e71
Glitch (hello-express) <none>
2021-04-18 20:11:20 +0000

    🎯🌜 Checkpoint ./public/client.js:26910881/3316 ./views/index.html:26910881/63 ./views/characters.html:26910881/1457

--- public/client.js
+++ public/client.js
@@ -1,42 +1,49 @@
 // client-side javascript
 class Client {
-  constructor() {
+  constructor(document) {
+    this.document = document;
     this.characters = {
       list: document.getElementById("characters"),
       form: document.querySelector("form")
     }
+    this.flags = {
+      new: true
+    }
+  }
+  appendCharacter(character) {
+    const newListItem = this.document.createElement("li");
+    newListItem.innerText = character;
+    this.characters.list.appendChild(newListItem);
+  }
+  filterCharacters(query) {
+    // fetch the initial list of docs
+    fetch("/mongo/characters")
+      .then(response => response.json()) // parse the JSON from the server
+      .then(characters => {
+        // remove the loading text
+        if (this.flags.new) {
+          this.characters.list.firstElementChild.remove();
+          this.flags.new = false;
+        }
+
+        // iterate through every dream and add it to our page
+        characters.forEach(ch => this.appendCharacter(ch));
+
+        // listen for the form to be submitted and add a new dream when it is
+        this.characters.form.addEventListener("submit", event => {
+          // stop our form submission from refreshing the page
+          event.preventDefault();
+
+          // get dream value and add it to the list
+          let newCharacter = this.characters.form.elements.new_character.value;
+          characters.push(newCharacter);
+          this.appendCharacter(newCharacter);
+
+          // reset form
+          this.characters.form.reset();
+          this.characters.form.elements.new_character.focus();
+        });
+      });
   }
 }
-
-// a helper function that creates a list item for a given dream
-function appendNewCharacter(character) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = character;
-  charactersList.appendChild(newListItem);
-}
-
-// fetch the initial list of docs
-fetch("/mongo/characters")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(characters => {
-    // remove the loading text
-    charactersList.firstElementChild.remove();
-
-    // iterate through every dream and add it to our page
-    characters.forEach(appendNewCharacter);
-
-    // listen for the form to be submitted and add a new dream when it is
-    charactersForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newCharacter = charactersForm.elements.new_character.value;
-      characters.push(newCharacter);
-      appendNewCharacter(newCharacter);
-
-      // reset form
-      charactersForm.reset();
-      charactersForm.elements.new_character.focus();
-    });
-  });
+const client = new Client(document);
