7efa2124fac424796e14c8368d5c111a62f7ef96
Glitch (hello-express) <none>
2021-04-18 19:36:58 +0000

    ⛳️💿 Checkpoint ./views/index.html:26910881/2683 ./public/assets/js/loader.js:26910881/2872

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -1,27 +1,33 @@
 class Loader {
   constructor() {}
-  addBanner(div) {
-    div.innerHTML = `
+  addBanner(document) {
+    const new_str = `
       <div class="container">
         <div class="row">
-          <div class="col-lg-6">
-            <h2>Join</h2>
-          </div>
-          <div class="col-lg-6">
-            <ul class="list-inline banner-social-buttons">
-              <li>
-                <a href="https://zmap-game.glitch.me/characters" class="btn btn-default btn-lg"><span class="network-name">Characters</span></a>
-              </li>
-              <li>
-                <a href="https://zmap-game.glitch.me/profile/login" class="btn btn-default btn-lg"><span class="network-name">Profile</span></a                  >
-              </li>
-              <li>
-                <a href="https://zmap-game.glitch.me/game" class="btn btn-default btn-lg"><span class="network-name">Play</span></a>
-              </li>
-            </ul>
+          <div class="col-lg-12">
+            <div class="intro-message">
+              <h1>ZMAP 5e Campaign</h1>
+              <h3>Saltmarsh: Dead Men Tell No Tales</h3>
+              <hr class="intro-divider" />
+                <ul class="list-inline intro-social-buttons">
+                  <li>
+                    <a href="https://zmap-game.glitch.me/characters" class="btn btn-default btn-lg"><span class="network-name">Characters</span></a>
+                  </li>
+                  <li>
+                    <a href="https://zmap-game.glitch.me/profile/login" class="btn btn-default btn-lg"><span class="network-name">Profile</span></a                  >
+                  </li>
+                  <li>
+                    <a href="https://zmap-game.glitch.me/game" class="btn btn-default btn-lg"><span class="network-name">Play</span></a>
+                  </li>
+                </ul>
+            </div>
           </div>
         </div>
-      </div>`;
+      </div>
+      `;
+    console.log("Writing new Banner HTML:");
+    console.log(new_str);
+    document.getElementById("about").innerHTML = new_str;
   }
 }
-const loader = Loader();
+const loader = new Loader();
