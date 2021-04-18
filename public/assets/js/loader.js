a6f21682f18a76064bfad8966face199239f5a42
Glitch (hello-express) <none>
2021-04-18 19:25:21 +0000

    🐽✊ Checkpoint ./public/assets/js/loader.js:26910881/4859 ./views/index.html:26910881/228

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -1,5 +1,27 @@
-module.exports = class Loader {
-  constructor() {
-    
+class Loader {
+  constructor() {}
+  addBanner(div) {
+    div.innerHTML = `
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h2>Join</h2>
+          </div>
+          <div class="col-lg-6">
+            <ul class="list-inline banner-social-buttons">
+              <li>
+                <a href="https://zmap-game.glitch.me/characters" class="btn btn-default btn-lg"><span class="network-name">Characters</span></a>
+              </li>
+              <li>
+                <a href="https://zmap-game.glitch.me/profile/login" class="btn btn-default btn-lg"><span class="network-name">Profile</span></a                  >
+              </li>
+              <li>
+                <a href="https://zmap-game.glitch.me/game" class="btn btn-default btn-lg"><span class="network-name">Play</span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
   }
 }
+const loader = Loader();
